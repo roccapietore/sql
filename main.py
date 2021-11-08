063c2971e01aa2bac816c83d22731291d842af14
Verevkina Elena <verevkinaee@gmail.com>
2021-11-08 11:49:54 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import sqlite3
-from flask import Flask
-import json
+from flask import Flask, abort, jsonify
 
 app = Flask(__name__)
 
 
-@app.route("/<id>")
-def get_animal_by_id(id):
+def get_record_by_id(record_id):
     con = sqlite3.connect("animal.db")
     cur = con.cursor()
-    sql = f"SELECT animal_id, age_upon_outcome, name,  animals_types, breed  FROM animals " \
-          f"where animal_id = '{id}'"
+    sql = f"""SELECT r.name, r.animal_id, b.breeds, r.age_upon_outcome
+               FROM results r 
+               LEFT JOIN animals_breeds b ON b.id = r.breed
+               where animal_id = '{record_id}'
+    """
     result = cur.execute(sql).fetchall()
-    result_dict = {
-        "animal_id": result[0][0],
-        "age_upon_outcome": result[0][1],
-        "name": result[0][2],
-        "animals_types": result[0][3],
-        "breed": result[0][4]
-    }
-    return json.dumps(result_dict)
+    if result:
+        return {
+            "name": result[0][0],
+            "animal_id": result[0][1],
+            "breeds": result[0][2],
+            "age_upon_outcome": result[0][3]
+        }
+
+
+@app.route('/<record_id>')
+def index(record_id):
+    result = get_record_by_id(record_id)
+    if not result:
+        abort(404)
+    return jsonify(result)
 
 
 if __name__ == '__main__':
